4cdab5e214f8cf81c1ffe644f73b041046753f37
dmpt1992 <developwebsitedanielamedeira@gmail.com>
2026-04-15 15:15:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-// smooth scroll
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener("click", function(e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href"))
-      .scrollIntoView({ behavior: "smooth" });
+const elements = document.querySelectorAll(".fade");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
   });
 });
+
+elements.forEach(el => observer.observe(el));
